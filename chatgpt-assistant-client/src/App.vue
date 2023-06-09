c583f2095d299bdbf2fc4c6054e8416d2907f9d2
qifan <1507906763@qq.com>
2023-06-09 22:06:16 +0800

    refactor: 页面过度效果

--- chatgpt-assistant-client/src/App.vue
+++ chatgpt-assistant-client/src/App.vue
@@ -14,20 +14,26 @@ import { RouterView } from 'vue-router'
 body {
   margin: 0;
 }
-.slide-leave-active,
 .slide-enter-active {
-  transition: all 0.5s ease-out;
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
 }
-
-.slide-enter-to,
-.slide-leave-to {
-  opacity: 0;
+.slide-leave-active {
+  transition: all 0.3s ease-out;
+}
+.slide-enter-from {
   transform: translateX(-100vw);
+  opacity: 0;
 }
-
-.slide-enter-from,
-.slide-leave-from {
-  opacity: 1;
+.slide-enter-to {
   transform: translateX(0);
+  opacity: 1;
+}
+.slide-leave-from {
+  transform: translateX(0);
+  opacity: 1;
+}
+.slide-leave-to {
+  transform: translateX(100vw);
+  opacity: 0;
 }
 </style>
