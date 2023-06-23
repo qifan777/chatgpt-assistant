34524a3b2fa5ee08b932a73cb8727de5f8fe4713
qifan <1507906763@qq.com>
2023-06-23 22:30:15 +0800

    feat: 第八期 Steam请求/Websocket推送

--- chatgpt-assistant-client/src/views/home/HomeView.vue
+++ chatgpt-assistant-client/src/views/home/HomeView.vue
@@ -1,7 +1,39 @@
 <script lang="ts" setup>
-import { onMounted } from 'vue'
-import router from '@/router'
+import { Client } from '@stomp/stompjs'
+import { ref } from 'vue'
+
+const result = ref('')
+const prompt = ref('')
+const client = new Client({
+  brokerURL: 'ws://localhost:8080/handshake',
+  onConnect: () => {
+    client.subscribe('/user/queue/chatMessage/receive', (message) => (result.value += message.body))
+  }
+})
+client.activate()
+const handleSend = () => {
+  client.publish({
+    destination: '/socket/chatMessage/send',
+    body: JSON.stringify({
+      session: { id: '6495a20647fbac571764c984' },
+      content: prompt.value,
+      role: 'user'
+    })
+  })
+  result.value = ''
+  prompt.value = ''
+}
 </script>
 <template>
-  <div>hello</div>
+  <div>
+    <el-form>
+      <el-form-item label="结果">
+        <el-input v-model="result" type="textarea"></el-input>
+      </el-form-item>
+      <el-form-item label="提问">
+        <el-input v-model="prompt"></el-input>
+      </el-form-item>
+      <el-button type="primary" @click="handleSend">发送</el-button>
+    </el-form>
+  </div>
 </template>
